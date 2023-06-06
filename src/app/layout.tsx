fbbdf85554c0c4d2bbf8f2de2db105fa7165e93f
Derek Wan <d.wan@icloud.com>
2023-06-06 14:36:39 +0800

    Finish lesson 5 (providers for auth and Supabase)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,30 @@
-import Sidebar from "./components/Sidebar";
+import Sidebar from "../components/Sidebar";
 import "./globals.css";
+import SupabaseProvider from "@/providers/SupabaseProvider";
+import UserProvider from "@/providers/UserProvider";
 import { Figtree } from "next/font/google";
 
 const font = Figtree({ subsets: ["latin"] });
 
 export const metadata = {
-	title: "Spotify Clone",
-	description: "Listen to music!",
+  title: "Spotify Clone",
+  description: "Listen to music!",
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-	return (
-		<html lang="en">
-			<body className={font.className}>
-				<Sidebar>{children}</Sidebar>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <SupabaseProvider>
+          <UserProvider>
+            <Sidebar>{children}</Sidebar>
+          </UserProvider>{" "}
+        </SupabaseProvider>
+      </body>
+    </html>
+  );
 }
