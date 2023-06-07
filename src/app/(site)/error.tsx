1c741510a6b66cde5f903ba1406dadffdc9c552e
Derek Wan <d.wan@icloud.com>
2023-06-07 22:46:25 +0800

    Finish lesson 11 (Stripe integration)

---
+++ src/app/(site)/error.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import Box from "../../components/Box";
+
+export default function Error() {
+  return (
+    <Box className="flex h-full items-center justify-center">
+      <div className="text-neutral-400">Something went wrong</div>
+    </Box>
+  );
+}
---
+++ src/app/liked/loading.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { BounceLoader } from "react-spinners";
+import Box from "../../components/Box";
+
+export default function Loading() {
+  return (
+    <Box className="flex h-full items-center justify-center">
+      <BounceLoader color="#22c55e" size={40} />
+    </Box>
+  );
+}
